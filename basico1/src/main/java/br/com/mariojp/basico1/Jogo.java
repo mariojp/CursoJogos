213a3974496a752827860a0ff29c9d77b36c951b
Mario Jorge Pereira <mariojp@gmail.com>
2016-05-22 23:38:15 -0300

    Basico 1

---
+++ basico1/src/main/java/br/com/mariojp/basico1/Jogo.java
@@ -0,0 +1,12 @@
+package br.com.mariojp.basico1;
+
+import javax.swing.JPanel;
+
+public class Jogo extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	public Jogo() {
+	}
+
+}
